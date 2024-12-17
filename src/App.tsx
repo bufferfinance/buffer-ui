c9d337cde2a05cea0307ff714b91dbee3658e41b
maxwell-1999 <124770082+maxwell-1999@users.noreply.github.com>
2024-12-17 12:20:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -356,15 +356,15 @@ const contents = {
     </a>
   ),
   default: (
-    <div className=" flex flex-col items-center justify-center gap-[30px]">
-     <div className="text-[40px]">
+    <div className=" flex flex-col items-center justify-center gap-[30px] leading-relaxed">
+     <div className="text-[40px] leading-relaxed">
        App is undergoing maintenance.
      </div>
-     <div className="text-[25px] flex-col flex items-center justify-center gap-[20px]">
+     <div className="leading-relaxed text-[25px] flex-col flex items-center justify-center gap-[20px]">
        <div>
        Launching Soon! </div>
-       <div className="text-[18px] flex text-center ">
-         HL Names are live. Connect the wallet and see your < br/ > HLName in Account Dropdown (if associated).
+       <div className="text-[18px] flex leading-relaxed text-center ">
+         HL Names are live. Connect the wallet and see your < br className="sm:hidden" / > HLName in Account Dropdown (if associated).
        </div>
      </div>
     </div>
