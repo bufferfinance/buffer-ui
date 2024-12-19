a4c639f166e6beca359f360cd1f83ed60d8ee440
maxwell-1999 <maxwell@buffer.finance>
2024-12-19 10:38:49 +0530

    WIP - replace contract call with API

--- src/Views/Common/Navbar/useHLName.ts
+++ src/Views/Common/Navbar/useHLName.ts
@@ -1,16 +1,28 @@
 import { HLNamesABI } from '@ABIs/HyperliquidNames.abi';
 import { useUserAccount } from '@Hooks/useUserAccount';
+import axios from 'axios';
 
 import useSWR from 'swr';
 import { useReadContract } from 'wagmi';
 //0xCCc9b4225edD38d3Ff1cC738efC2e6bDEeF241F2.primaryName([address]);
 export const useHLName = () => {
   const { address } = useUserAccount();
-  const response = useReadContract({
-    address: '0xCCc9b4225edD38d3Ff1cC738efC2e6bDEeF241F2',
-    abi: HLNamesABI,
-    functionName: 'primaryName',
-    args: [address],
+  const { data } = useSWR(['dssdfdsa', address], {
+    fetcher: async () => {
+      if (!address) return null;
+      const resp = await axios.get(
+        `https://hlnames-rest-api.onrender.com/api/resolve/primary_name/${address}`,
+        {
+          headers: {
+            Accept: '*/*',
+            'X-API-Key': 'CPEPKMI-HUSUX6I-SE2DHEA-YYWFG5Y',
+          },
+        }
+      );
+      console.log('response', resp);
+      return resp?.data?.primaryName;
+    },
   });
-  return response.data;
+  console.log("data'", data);
+  return data;
 };
